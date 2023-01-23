f0ac511784bb4ed77e399977928eecb1c03dc643
Israelartinez-ATOS <johan-israel.martinezalarcon.external@atos.net>
2023-01-23 09:10:49 -0600

    230123

---
+++ JS/JS-variables/guia.js
@@ -0,0 +1,54 @@
+//██╗   ██╗ █████╗ ██████╗ ██╗ █████╗ ██████╗ ██╗     ███████╗███████╗
+//██║   ██║██╔══██╗██╔══██╗██║██╔══██╗██╔══██╗██║     ██╔════╝██╔════╝
+//██║   ██║███████║██████╔╝██║███████║██████╔╝██║     █████╗  ███████╗
+//╚██╗ ██╔╝██╔══██║██╔══██╗██║██╔══██║██╔══██╗██║     ██╔══╝  ╚════██║
+// ╚████╔╝ ██║  ██║██║  ██║██║██║  ██║██████╔╝███████╗███████╗███████║
+//  ╚═══╝  ╚═╝  ╚═╝╚═╝  ╚═╝╚═╝╚═╝  ╚═╝╚═════╝ ╚══════╝╚══════╝╚══════╝
+
+/*
+Existen tipos de variables:
+    undefined:
+        Tipo de variable que aun no ha sido definida(aun no se ha determinado su uso).
+    null:
+        null por su significado del ingles es "nada", por lo que es un tipo de dato que no hace nada.
+    boolean:
+        variable de tipo "verdadero(true)" o "falso(false)".
+    string:
+        Cadena de cualquier tipo de texto.
+    symbol:
+        Un solo simbolo de valor primitivo inmutable que es único.
+    number:
+        Un valor de forma numerica(cantidad).
+    object:
+        Objeto que puede almacenar muchos tipos de valores diferentes.
+
+En total son 7 tipos de datos que JS brinda.
+*/
+
+var miNombre = "Israel"
+// "var significa "Variable", y como podemos ver es un tipo de variable "string" por el texto como valor de esta.
+
+miNombre = 8
+// En este caso la variable pasa a ser "number" por el tipo de dato numerico(8) que esta en esta. Tampoco se necesito usar "var" por que la variable ya esta declarada.
+
+let otroNombre = "Johan"
+// Este es otro tipo de declarar una variable, pero con Let.
+
+const pi = 3.14
+// Este es otro tipo de mandar a declarar una variable, en este caso la variable sera constante.
+
+
+var suma = 1;
+suma++; // esto hace que cada que el programa lea esta linea de codigo, la variable "suma" se sumara 1. En lugar de usar "suma = suma + 1;"
+suma --; // tambien funciona de forma contraria con "--"
+
+
+
+
+//███████╗███████╗ ██████╗██╗   ██╗███████╗███╗   ██╗ ██████╗██╗ █████╗ ███████╗    ██████╗ ███████╗    ███████╗███████╗ ██████╗ █████╗ ██████╗ ███████╗
+//██╔════╝██╔════╝██╔════╝██║   ██║██╔════╝████╗  ██║██╔════╝██║██╔══██╗██╔════╝    ██╔══██╗██╔════╝    ██╔════╝██╔════╝██╔════╝██╔══██╗██╔══██╗██╔════╝
+//███████╗█████╗  ██║     ██║   ██║█████╗  ██╔██╗ ██║██║     ██║███████║███████╗    ██║  ██║█████╗      █████╗  ███████╗██║     ███████║██████╔╝█████╗  
+//╚════██║██╔══╝  ██║     ██║   ██║██╔══╝  ██║╚██╗██║██║     ██║██╔══██║╚════██║    ██║  ██║██╔══╝      ██╔══╝  ╚════██║██║     ██╔══██║██╔═══╝ ██╔══╝  
+//███████║███████╗╚██████╗╚██████╔╝███████╗██║ ╚████║╚██████╗██║██║  ██║███████║    ██████╔╝███████╗    ███████╗███████║╚██████╗██║  ██║██║     ███████╗
+//╚══════╝╚══════╝ ╚═════╝ ╚═════╝ ╚══════╝╚═╝  ╚═══╝ ╚═════╝╚═╝╚═╝  ╚═╝╚══════╝    ╚═════╝ ╚══════╝    ╚══════╝╚══════╝ ╚═════╝╚═╝  ╚═╝╚═╝     ╚══════╝
+
