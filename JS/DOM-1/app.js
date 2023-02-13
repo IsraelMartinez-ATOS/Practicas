bb49cb32c8c5f01a469c71eda11c0d253dee8a71
Israelartinez-ATOS <johan-israel.martinezalarcon.external@atos.net>
2023-02-13 10:30:35 -0600

    nuevo

---
+++ JS/DOM-1/app.js
@@ -0,0 +1,12 @@
+let booklist = document.getElementsByClassName('book-list');
+let name1 = document.getElementsByClassName("name1");
+let name2 = document.getElementsByClassName("name2");
+let name3 = document.getElementsByClassName("name3");
+let name4 = document.getElementsByClassName("name4");
+let delete1 = document.getElementsByClassName("delete1");
+let delete2 = document.getElementsByClassName("delete2");
+let delete3 = document.getElementsByClassName("delete3");
+let delete4 = document.getElementsByClassName("delete4");
+
+
+
