f9b868f096879ff6ccfb8ba301d1ae13b7656023
Israelartinez-ATOS <johan-israel.martinezalarcon.external@atos.net>
2023-04-05 09:34:04 -0600

    TS

---
+++ TypeScript/TS-1/index.js
@@ -0,0 +1,22 @@
+async function hello() {
+    return 'world';
+}
+let lucky = 23;
+export {};
+//objetos
+/*interface person{
+    first:string;
+    last:string;
+    [key: string] any
+}
+
+const person: Person = {
+    first:'bob',
+    last:'Delaney'
+}
+
+const person{
+    first:'Usain',
+    last:'bolt',
+    key: true
+}*/
---
+++ TypeScript/TS-1/index.ts
@@ -0,0 +1,27 @@
+import * as _ from 'lodash'
+
+async function hello(){
+    return 'world'
+}
+
+let lucky = 23;
+
+//objetos
+
+/*interface person{
+    first:string;
+    last:string;
+    [key: string] any
+}
+
+const person: Person = {
+    first:'bob',
+    last:'Delaney'
+}
+
+const person{
+    first:'Usain',
+    last:'bolt',
+    key: true
+}*/
+
